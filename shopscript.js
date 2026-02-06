1613c173c8ea88351e279cafd8b4459d3720441c
Rebeckawestborg <rwestborg@gmail.com>
2026-02-06 12:10:04 +0100

    siste oppdatering

--- shopscript.js
+++ shopscript.js
@@ -18,10 +18,65 @@ function fetchProduct() {
 
 fetchProduct()
 
+//Generer handlevogn
+function showCart() {
+    //Unike produkter
+    let uniqueItems = new Set(cart) 
+    let uniqueArray = [...uniqueItems]
+    //Oversikt over antall per produkt
+    let cartItems = []
+    uniqueArray.map(item => {
+        cartItems.push({prodid: item, quantity: cart.filter(i => i === item).length})
+    })
+
+    //gå gjennom cartItems for å lage HTML til handlekurven og regne ut totalpris
+    let cartHTML = ""
+    let totalPrice = 0
+
+    cartItems.map(ci => {
+        //hente produktinformasjon
+        let product = products.find(i => i.prodid === ci.prodid)
+
+        //skrive ut HTML
+        cartHTML += `<tr>
+                    <td class="title">${product.title}</td>
+                    <td class="price">${product.price}</td>
+                    <td class="quantity">${ci.quantity}</td>
+                    <td class="delete"><button onClick="deleteFromCart(${product.prodid})">X</button></td>
+                </tr>`
+        //summere totalpris
+        totalPrice += Number(product.price) * Number(ci.quantity)
+
+    })
+
+    if(cart.length === 0) {
+        cartHTML += "<tr><td>Ingen varer i handlevognen.</td></tr>"
+    }
+    //oppdatere HTML-elementer
+    document.getElementById("cart-items").innerHTML = cartHTML
+    document.getElementById("total-price").innerHTML = totalPrice
+    document.getElementById("cart-quantity").innerHTML = cart.length
+}
+
+//slette fram handlevogn
+function deleteFromCart(prodid) {
+    let deleteIndex = cart.indexOf(prodid)
+    if(deleteIndex > -1) {
+        cart.splice(deleteIndex, 1)
+    }
+    //oppdatere handlevogn-utskrift
+    showCart()
+}
+
+
 //legg til i handlevogn
 function addToCart(prodid) {
     console.log("Legg til produkt med id: " + prodid)
     cart.push(prodid)
     console.log(cart)
-    document.getElementById("cart-quantity").innerHTML = cart.length
+    
+    //oppdatere handlevogn-visning:
+
+    showCart()
 }
+
